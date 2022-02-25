8627ff99a575cfa181829e1906bdaa8c176ee3a7
Evgenii Kolesnikov <jekow@yandex.ru>
2022-02-25 21:33:01 +0300

    Add solution 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,41 @@
-﻿// Task 1
+﻿using System;
 
-const string x = "aabbcccdde";
-string target = "a2b2c3d2e1";
+class Task1
+{
+  static void Main(string[] args)
+  {
+    // Task 1
+    const string x = "aabbcccdde";
+    // string target = "a2b2c3d2e1";
+    Console.WriteLine("Init   = " + x);
+    Console.WriteLine("Result = " + MagicBox(x));
+  }
+
+  // Solution
+  static string MagicBox(string x)
+  {
+    string res = "";
+    int cnt = 1;
+    for (int i = 0; i < x.Length; i++)
+    {
+      try
+      {
+        if (x[i] == x[i + 1])
+        {
+          cnt++;
+        }
+        else
+        {
+          res += x[i] + cnt.ToString();
+          cnt = 1;
+        }
+      }
+      catch (IndexOutOfRangeException)
+      {
+        res += x[i] + cnt.ToString();
+        cnt = 1;
+      }
+    }
+    return res;
+  }
+}
