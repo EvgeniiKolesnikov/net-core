87331f2f00d3021bb7c3279a1926be84777fb805
Evgenii Kolesnikov <jekow@yandex.ru>
2022-02-26 01:05:34 +0300

    Format code

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,35 +1,28 @@
 ﻿using System;
 
-class Task1
-{
-  static void Main(string[] args)
-  {
-    // Task 1
-    const string x = "aabbcccdde";
-    // string target = "a2b2c3d2e1";
-    Console.WriteLine("Init   = " + x);
-    Console.WriteLine("Result = " + MagicBox(x));
-  }
-
-  // Solution
-  static string MagicBox(string word)
-  {
-    string res = "";
-    int count = 1;
-
-    for (int i = 0; i < word.Length - 1; i++)
-    {
-      if (word[i] == word[i + 1])
-      {
-        count++;
-      }
-      else
-      {
-        res += word[i] + count.ToString();
-        count = 1;
-      }
+class Task1 {
+    static void Main(string[] args) {
+        // Task 1
+        const string x = "aabbcccdde";
+        // string target = "a2b2c3d2e1";
+        Console.WriteLine("Init   = " + x);
+        Console.WriteLine("Result = " + MagicBox(x));
     }
-    res += word[word.Length - 1] + count.ToString();
-    return res;
-  }
+
+    // Solution
+    static string MagicBox(string word) {
+        string res = "";
+        int count = 1;
+
+        for (int i = 0; i < word.Length - 1; i++) {
+            if (word[i] == word[i + 1]) {
+                count++;
+            } else {
+                res += word[i] + count.ToString();
+                count = 1;
+            }
+        }
+        res += word[word.Length - 1] + count.ToString();
+        return res;
+    }
 }
